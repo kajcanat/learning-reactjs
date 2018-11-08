634231fbf3155b3bb789f6e0ee76e452d6e916b5
kaj canat <jcanat@outlook.fr>
2018-11-08 12:01:12 +0100

    life cycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
 
 class App extends React.Component {
-   constructor() {
-    super();
-    this.state = {
-      header: "Header from state...",
-      content: "Content from state",
-      data: 
-      [
-	{"id":1, name:"John", age:21},
-	{"id":2, name:"Bob", age:31},
-	{"id":3, name:"Francine", age:41}
-      ]
-    } 
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         data: 0
+      }
+      this.increment = this.increment.bind(this)
+   };
+   increment() {
+      this.setState({data: this.state.data + 1})
    }
    render() {
       return (
          <div>
-            <Header/>
-            <table>
-		<tbody>
-		{this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-		</tbody>
-	    </table>
-	    <div>
-	     <h1>{this.state.header}</h1>
-	     <h1>{this.state.content}</h1>
-	    </div>
+	    <Header />
+            <button onClick = {this.increment}>INCREMENT</button>
+            <Content myNumber = {this.state.data}></Content>
          </div>
       );
    }
@@ -35,30 +26,35 @@ class Header extends React.Component {
    render() {
       return (
          <div>
-            <h1>Header</h1>
+            <h1>Lifecyle</h1>
          </div>
       );
    }
-}
-class TableRow extends React.Component {
-  render() {
-    return (
-	<tr>
-	<td>{this.props.data.id}</td>
-	<td>{this.props.data.name}</td>
-	<td>{this.props.data.age}</td>
-	</tr>
-    );
-  }
 }
 class Content extends React.Component {
-   render() {
-      return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
-         </div>
-      );
+   componentWillMount() {
+     console.log('component will mount')
    }
+   componentDidMount() { 
+     console.log('component did mount')
+   }
+   componentWillReceiveProps(newProps) {
+     console.log('Component will receive props');
+   }
+   shouldComponentUpdate(newProps, newState) {
+     return true;
+   }
+   componentWillUpdate(nextProps, nextState) {
+     console.log('Component will update');
+   }
+   componentDidUpdate(prevProps, prevState) {
+     console.log('component did update');
+   }
+   componentWillUnmount() {
+    console.log('component will unmount'); 
+   }
+  render() {
+    return (<div><h3>{this.props.myNumber}</h3></div>);
+  }
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 ReactDOM.render(<App />, document.getElementById('root'));
+
+setTimeout(() => {
+   ReactDOM.unmountComponentAtNode(document.getElementById('root'));}, 5000);
+
