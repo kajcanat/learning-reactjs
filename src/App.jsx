a38cb049e6d4dc95307f3737c7c63acc5f83e5f0
kaj canat <jcanat@outlook.fr>
2018-11-08 11:07:45 +0100

    forceUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -3,60 +3,16 @@ import React from 'react';
 class App extends React.Component {
    constructor() {
     super();
-    this.state = {
-      header: "Header from state...",
-      content: "Content from state",
-      data: 
-      [
-	{"id":1, name:"John", age:21},
-	{"id":2, name:"Bob", age:31},
-	{"id":3, name:"Francine", age:41}
-      ]
-    } 
+    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
    }
+   forceUpdateHandler() {
+     this.forceUpdate();
+   };
    render() {
       return (
          <div>
-            <Header/>
-            <table>
-		<tbody>
-		{this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-		</tbody>
-	    </table>
-	    <div>
-	     <h1>{this.state.header}</h1>
-	     <h1>{this.state.content}</h1>
-	    </div>
-         </div>
-      );
-   }
-}
-class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-   }
-}
-class TableRow extends React.Component {
-  render() {
-    return (
-	<tr>
-	<td>{this.props.data.id}</td>
-	<td>{this.props.data.name}</td>
-	<td>{this.props.data.age}</td>
-	</tr>
-    );
-  }
-}
-class Content extends React.Component {
-   render() {
-      return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
+	<button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
+	<h4>Random number: {Math.random()}</h4>
          </div>
       );
    }
