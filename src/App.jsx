93e88923c0e41aaef4c2eca5ffb5f8a27908e3be
kaj canat <jcanat@outlook.fr>
2018-11-08 12:33:45 +0100

    forms complex

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
-   constructor() {
-    super();
+   constructor(props) {
+    super(props);
     this.state = {
-      header: "Header from state...",
-      content: "Content from state",
-      data: 
-      [
-	{"id":1, name:"John", age:21},
-	{"id":2, name:"Bob", age:31},
-	{"id":3, name:"Francine", age:41}
-      ]
-    } 
+      data: 'Apple'
+    }
+    this.updateState = this.updateState.bind(this);
+   };
+   updateState(e) {
+     this.setState({data: e.target.value});
    }
    render() {
       return (
          <div>
             <Header/>
-            <table>
-		<tbody>
-		{this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-		</tbody>
-	    </table>
-	    <div>
-	     <h1>{this.state.header}</h1>
-	     <h1>{this.state.content}</h1>
-	    </div>
+	    <Content myDataProp = {this.state.data} updateStateProp = {this.updateState} />
          </div>
       );
    }
@@ -35,28 +24,17 @@ class Header extends React.Component {
    render() {
       return (
          <div>
-            <h1>Header</h1>
+            <h1>Forms complex</h1>
          </div>
       );
    }
-}
-class TableRow extends React.Component {
-  render() {
-    return (
-	<tr>
-	<td>{this.props.data.id}</td>
-	<td>{this.props.data.name}</td>
-	<td>{this.props.data.age}</td>
-	</tr>
-    );
-  }
 }
 class Content extends React.Component {
    render() {
       return (
          <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
+	   <input type="text" value={this.props.myDataProp} onChange={this.props.updateStateProp}/>
+           <h3>{this.props.myDataProp}</h3>
          </div>
       );
    }
